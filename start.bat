aac04f2dd700ed50e417bc8787797f2323c83eb4
LucasLK <122848089+LKzinxyz@users.noreply.github.com>
2023-06-03 23:13:58 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,11 @@
+@echo off
+REM Exibe o ASCII art
+color 0A
+mode con: cols=65 lines=8
+echo.
+echo           +---------------------------------------------+
+echo                           Bot working        
+echo           +---------------------------------------------+ 
+
 node index.js
+pause
